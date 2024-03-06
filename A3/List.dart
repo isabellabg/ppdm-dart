eeeb3112b11d5408dbf33a13914cb6b05b3575f0
isa borges <133895026+isabellabg@users.noreply.github.com>
2024-03-06 09:27:06 -0300

    Add files via upload

---
+++ A3/List.dart
@@ -0,0 +1,9 @@
+void main() {
+  List<String> frutas = ["banana", "maçã", "laranja", "abacaxi", "kiwi"];
+
+  frutas.sort();
+
+  print("Lista de frutas em ordem alfabética");
+  for (String string in frutas) print(string);
+  {}
+}
